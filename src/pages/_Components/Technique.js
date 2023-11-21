5a39252596689e5a2c0914f9050c801c4fc2ac7f
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2023-11-21 22:54:30 +0100

    commit hh

--- src/pages/_Components/Technique.js
+++ src/pages/_Components/Technique.js
@@ -8,11 +8,20 @@ function Technique() {
 				<div className="md:w-1/2 ">
 					<h1 className="text-3xl md:px-5 indent-10">Nos techniques</h1>
 					<p className=" md:px-5 pt-4 font-light indent-2 ">
-						
-							"La moquette de pierre, aussi appelée moquette de marbre ou tapis de pierre, est un revêtement de sol qui convient aussi bien en intérieur qu'en extérieur, pour les particuliers comme les professionnels, Propriétaires de restaurant, hoteliers, gérants de camping, propriétaires de concessions automobiles, piscinistes... vous pouvez opter pour un revêtement de sol professionnel en marbre résiné afin d'habiller vos locaux. Nous accompagnons les professionnels pour réaliser un projet de A à Z afin d'habiller un espace intérieur ou extérieur"
-						
+						La moquette de pierre, aussi appelée moquette de marbre ou tapis de
+						pierre, est un revêtement de sol qui convient aussi bien en
+						intérieur qu&apos;en extérieur, pour les particuliers comme les
+						professionnels, Propriétaires de restaurant, hoteliers, gérants de
+						camping, propriétaires de concessions automobiles, piscinistes...
+						vous pouvez opter pour un revêtement de sol professionnel en marbre
+						résiné afin d&apos;habiller vos locaux. Nous accompagnons les
+						professionnels pour réaliser un projet de A à Z afin d&apos;habiller un
+						espace intérieur ou extérieur. 
 					</p>
-					<Link href={"/entreprise/technique"} className=" text-white hover:text-white border mx-5 border-gray-800 hover:bg-gray-900 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
+					<Link
+						href={"/entreprise/technique"}
+						className=" text-white hover:text-white border mx-5 border-gray-800 hover:bg-gray-900 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+					>
 						Learn more
 					</Link>
 				</div>
